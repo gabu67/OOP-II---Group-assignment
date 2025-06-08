17e5ff4197f4b3f45ec5998e3120ca9e2e01c5c4
Gabriel Osugo Kabese <gabriel.kabese@strathmore.edu>
2025-06-08 17:48:54 +0300

    InterfaceSegregation

---
+++ Gabriel & Brian's work/src/interface_segregation/InterfaceSegregation.java
@@ -0,0 +1,48 @@
+// Interface Segregation Principle Example
+
+interface Eater {
+    void eat();
+}
+
+interface Flyer {
+    void fly();
+}
+
+interface Swimmer {
+    void swim();
+}
+
+class Dog implements Eater, Swimmer {
+    public void eat() {
+        System.out.println("Dog eats.");
+    }
+
+    public void swim() {
+        System.out.println("Dog swims.");
+    }
+}
+
+class Bird implements Eater, Flyer {
+    public void eat() {
+        System.out.println("Bird eats.");
+    }
+
+    public void fly() {
+        System.out.println("Bird flies.");
+    }
+}
+
+public class InterfaceSegregation {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+
+        System.out.println("Dog's behavior:");
+        dog.eat();
+        dog.swim();
+
+        System.out.println("\nBird's behavior:");
+        bird.eat();
+        bird.fly();
+    }
+}
