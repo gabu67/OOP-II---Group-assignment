02783eeefd1fd4a0bf6b861c52f45f0162e114ee
gabu67 <gabriel.kabese@strathmore.edu>
2025-06-08 17:15:34 +0300

    Update InterfaceSegregationDemo.java

--- Gabriel and Brian's work/src/interface_segregation/Example2.java
+++ Gabriel and Brian's work/src/interface_segregation/Example2.java
@@ -1,7 +1,50 @@
-package interface_segregation;
+// Interface Segregation Principle Example
+interface Eater {
 
-public class Example2 {
-    /*
-    You can delete this class
-     */
+    void eat();
 }
+
+interface Flyer {
+    void fly();
+}
+
+interface Swimmer {
+    void swim();
+}
+
+class Dog implements Eater, Swimmer {
+    public void eat() {
+        System.out.println("Dog eats.");
+    }
+
+    public void swim() {
+        System.out.println("Dog swims.");
+    }
+}
+
+class Bird implements Eater, Flyer {
+    public void eat() {
+        System.out.println("Bird eats.");
+    }
+
+    public void fly() {
+        System.out.println("Bird flies.");
+    }
+}
+
+public class InterfaceSegregationDemo {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+
+        System.out.println("Dog's behavior:");
+        dog.eat();
+        dog.swim();
+
+        System.out.println("\nBird's behavior:");
+        bird.eat();
+        bird.fly();
+    }
+}
+
